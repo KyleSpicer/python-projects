ff852ec6d704bb5a40269903f212fe05c0b0a30f
Kyle Spicer <kyle.d.spicer.mil@army.mil>
2022-12-09 08:26:08 -0500

    add games repo

---
+++ games/flappybird/flappy.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+import pygame
+import sys
+import random
+
+
+def main():
+    pygame.init() # initializes pygame
+    screen = pygame.display.set_mode((576, 1024))
+    
+    # load images
+    background = pygame.image.load("./background.jpg").convert()
+    background = pygame.transform.scale2x(background)
+    
+    bird = pygame.image.load("./santa.jpg").convert()
+    
+    
+    while True:
+        for event in pygame.event.get(): # produces exit option on window
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        screen.blit(background,(0,0))
+        screen.blit(bird,(0,0))
+        
+        pygame.display.update()
+    
+    
+if __name__ == "__main__":
+    main()
